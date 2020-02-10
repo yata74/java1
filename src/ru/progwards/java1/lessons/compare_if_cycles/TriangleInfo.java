5d3a08831b9ad3648b06c704ea09028ee8365663
yata74 <yata74@rambler.ru>
2020-02-10 14:40:29 +0500

    TriangleInfo

---
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -0,0 +1,24 @@
+package ru.progwards.java1.lessons.compare_if_cycles;
+
+public class TriangleInfo {
+    public static boolean isTriangle(int a, int b, int c) {
+        boolean result = a < (b + c) && b < (a + c) && c < (b + a);
+        return result;
+    }
+    public static boolean isRightTriangle(int a, int b, int c){
+        boolean result = (a*a)==(b*b)+(c*c) || (b*b)==(c*c)+(a*a) || (c*c) == (b*b)+(a*a);
+        return result;
+    }
+    public static boolean isIsoscelesTriangle(int a, int b, int c){
+        boolean result = a==b || a==c || b==c;
+        return result;
+    }
+    public static void main(String[] args) {
+        boolean a1 = isTriangle(3,4,5);
+        System.out.println(a1);
+        boolean a2 = isRightTriangle(12,13,5);
+        System.out.println(a2);
+        boolean a3 = isIsoscelesTriangle(4,5,5);
+        System.out.println(a3);
+    }
+}
