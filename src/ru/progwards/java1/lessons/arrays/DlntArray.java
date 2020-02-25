f6e06dd141d78dadeda77c0a4201842aa714c7a1
yata74 <yata74@rambler.ru>
2020-02-25 18:07:30 +0500

    Eratosthenes

---
+++ src/ru/progwards/java1/lessons/arrays/DlntArray.java
@@ -0,0 +1,59 @@
+package ru.progwards.java1.lessons.arrays;
+
+import java.util.Arrays;
+
+public class DlntArray {
+    private int[] mass;
+
+    public DlntArray() {
+    }
+
+    public void add(int num) {
+        int[] mass1 = new int[mass.length + 1];
+        for (int i = 0; i < mass.length; i++) {
+            mass1[i] = mass[i];
+        }
+        mass1[mass1.length - 1] = num;
+        System.out.println(Arrays.toString(mass1));
+    }
+
+    public void atInsert(int pos, int num) {
+        int[] mass2 = new int[mass.length + 1];
+        for (int i = 0; i < mass2.length; i++) {
+            if (i < pos) {
+                mass2[i] = mass[i];
+            } else if (i == pos) {
+                mass2[i] = num;
+            } else mass2[i] = mass[i - 1];
+        }
+        System.out.println("mass2" + Arrays.toString(mass2));
+
+    }
+
+    public void atDelete(int pos) {
+        int[] mass3 = new int[mass.length - 1];
+        for (int i = 0; i < mass3.length; i++) {
+            if (i < pos) {
+                mass3[i] = mass[i];
+            } else mass3[i] = mass[i + 1];
+        }
+        System.out.println(Arrays.toString(mass3));
+    }
+
+    public int at(int pos) {
+        int res = mass[pos];
+        return res;
+    }
+
+
+    public static void main(String[] args){
+        DlntArray mass4 = new  DlntArray();
+        mass4.mass = new int[]{3, 6, 7,9,1};
+        mass4.add(5);
+        mass4.atInsert(3,8);
+        mass4.atDelete(2);
+        int a= mass4.at(3);
+        System.out.println("на позиции 3 число "+ a);
+    }
+
+}
---
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -0,0 +1,33 @@
+package ru.progwards.java1.lessons.arrays;
+
+import java.util.Arrays;
+
+public class Eratosthenes {
+    private boolean[] sieve;
+    public Eratosthenes(int N) {
+        int a = N;
+        sieve = new boolean[a];
+        Arrays.fill(sieve, true);
+        sift();
+    }
+
+    private void sift (){
+        for (int i = 2; i < sieve.length+1; i++) {
+           for (int j=2; j*i< sieve.length+1; j++){
+                if (sieve [j*i-1] == true) {
+                    sieve[j*i-1] = false;
+                }
+            }
+        }
+    }
+
+    public boolean isSimple(int n1) {
+        boolean a2 = sieve[n1];
+        return a2;
+    }
+    public static void main(String[] args){
+    Eratosthenes Eratosthenes1 = new Eratosthenes(20);
+        System.out.println(Arrays.toString(Eratosthenes1.sieve));
+        System.out.println(Eratosthenes1.isSimple(10));
+}
+}
