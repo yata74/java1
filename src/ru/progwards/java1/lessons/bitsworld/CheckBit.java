4f6477ec6a7f51791f7732d907669c0b00f15020
yata74 <yata74@rambler.ru>
2020-02-28 12:23:51 +0500

    CheckBit

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -1,1 +1,17 @@
+package ru.progwards.java1.lessons.bitsworld;
 
+public class CheckBit {
+    public static int checkBit(byte value, int bitNumber){
+        for (int i=0; i<bitNumber; i++){
+            value >>= 1;
+        }
+        byte x = 0b00000001;
+        x &= value;
+        if (x == 0b00000001) {
+            return 1;
+        }return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println(checkBit((byte) 0b01110111,5));
+    }
+}
