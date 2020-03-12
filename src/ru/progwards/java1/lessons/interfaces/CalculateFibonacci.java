bd6a8b2ce89ccd7b171254d54b218c88d7b8e343
yata74 <yata74@rambler.ru>
2020-03-12 16:50:25 +0500

    CalculateFibonacci

---
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -0,0 +1,57 @@
+package ru.progwards.java1.lessons.interfaces;
+
+public class CalculateFibonacci {
+    private static CacheInfo lastFibo;
+
+
+    //рассчитываем число Фибоначи
+    public static int fiboNumber(int n) {
+
+        if (lastFibo==null || n != lastFibo.n){
+            int sum = 0;
+            int b1;
+            int b2 = 1;
+            int i = 1;
+            do {
+                b1 = b2;
+                b2 = sum;
+                sum = b1 + b2;
+                i++;
+            } while (i <= n);
+            System.out.println("Расчет");
+            lastFibo.fibo = sum;
+            return sum;
+        }else {
+            System.out.println("повтор");
+            return lastFibo.fibo;
+
+        }
+
+    }
+
+    //вложенный класс
+    public static class CacheInfo {
+        int n;
+        int fibo;
+
+        public CacheInfo(int n, int fibo) {
+            this.n = n;
+            this.fibo = fibo;
+        }
+
+        public static CacheInfo getLastFibo() {
+            return lastFibo;
+        }
+
+        public static void clearLastFibo() {
+            lastFibo = null;
+        }
+
+    }
+    public static void main(String[] args){
+        System.out.println(fiboNumber(7));
+        System.out.println(fiboNumber(3));
+        System.out.println(fiboNumber(3));
+
+    }
+}
