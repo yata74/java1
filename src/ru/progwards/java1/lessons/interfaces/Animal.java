ec2341f70b63406dc0aeb4b0802d00d2fe7bc6c3
yata74 <yata74@rambler.ru>
2020-03-14 15:21:57 +0500

    Animal

--- src/ru/progwards/java1/lessons/interfaces/Animal.java
+++ src/ru/progwards/java1/lessons/interfaces/Animal.java
@@ -1,9 +1,10 @@
 package ru.progwards.java1.lessons.interfaces;
 
 
+import java.util.Arrays;
 import java.util.Objects;
 
-public class Animal implements FoodCompare {
+public class Animal implements FoodCompare, CompareWeight {
 
     public enum FoodKind {
         UNKNOWN,
@@ -69,7 +70,7 @@ public class Animal implements FoodCompare {
         if (anObject == null || getClass() != anObject.getClass())
             return false;
 
-         Animal obj = (Animal) anObject;
+        Animal obj = (Animal) anObject;
 
         return Double.compare(obj.getWeight(), this.getWeight()) == 0;
 
@@ -91,25 +92,57 @@ public class Animal implements FoodCompare {
     public double getFoodPrice(){
         return (calculateFoodWeight() * getFood1kgPrice());
     }
-  //Сравнение цен на еду у разных животных
-  @Override
-  public int compareFoodPrice(Animal animal){
-      return (Double.compare(this.getFoodPrice(),animal.getFoodPrice()));
-  }
+    //Сравнение цен на еду у разных животных
+    @Override
+    public int compareFoodPrice(Animal animal){
+        return (Double.compare(this.getFoodPrice(),animal.getFoodPrice()));
+    }
+
+    //Сравнение веса
+
+//    @Override
+//    public CompareResult compareWeight(CompareWeight smthHasWeigt){
+//
+//          Animal ani = (Animal) smthHasWeigt;
+//        double a1 = Double.compare(this.weight,ani.getWeight());
+//        if (a1<0){ return CompareResult.LESS; }
+//        if (a1==0){return CompareResult.EQUAL;}
+//        else return CompareResult.GREATER;
+//    }
+
+//    public static void sort(CompareWeight[] a){
+//            for (int i=0; i<a.length; i++){
+//                for (int j=i; j<a.length; j++){
+//                    if (a[i].compareWeight(a[j])==CompareResult.GREATER)  {
+//                        CompareWeight b = a[i];
+//                        a[i] = a[j];
+//                        a[j]= b;
+//                    }
+//                }
+//            }
+//        }
+
+
 
 
     public static void main(String[] args) {
         Cow cow1 = new Cow(14);
         Cow cow2 = new Cow(14);
+        Hamster hamster1 = new Hamster(14);
+        Hamster hamster2 = new Hamster(2);
+        Duck duck1 = new Duck(3);
+        Duck duck2 = new Duck(5);
+        CompareWeight[] a = {cow1,cow2,hamster1,hamster2,duck1,duck2};
         System.out.println(cow1.equals(cow2));
         System.out.println(cow1.toStringFull());
         System.out.println(cow2.toStringFull());
-        Hamster hamster1 = new Hamster(14);
-        Hamster hamster2 = new Hamster(0.7);
         System.out.println(hamster1.equals(cow2));
         System.out.println(hamster1.toStringFull());
         System.out.println(hamster2.toStringFull());
         System.out.println(cow1.compareFoodPrice(hamster1));
+        System.out.println(cow1.compareWeight(hamster2));
+        CompareWeight.sort(a);
+        System.out.println(Arrays.toString(a));
     }
 
 }
