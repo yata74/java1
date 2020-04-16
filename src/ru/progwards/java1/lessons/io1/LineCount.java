dc7a2169b9a6a9d50424ea11d6d9d7b6bd325f5d
yata74 <yata74@rambler.ru>
2020-04-16 12:35:04 +0500

    LineCount

---
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -0,0 +1,38 @@
+package ru.progwards.java1.lessons.io1;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LineCount {
+    public static int calcEmpty(String fileName) throws Exception {
+        int i = 0;
+        try {
+            FileReader file1 = new FileReader(fileName);
+            Scanner scan = new Scanner(file1);
+            try {
+                while (scan.hasNextLine()) {
+                    String a1 = scan.nextLine();
+                    if (a1.equals("")) {
+                    i = i + 1;
+                      }
+                }
+            }finally {
+                file1.close();
+            }
+            return i;
+        } catch (Exception e) {
+            return -1;
+
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            System.out.println(calcEmpty("text1.txt"));
+
+        } catch (Exception e) {
+            System.out.println(e);;
+        }
+    }
+
+}
