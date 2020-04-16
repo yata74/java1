0ef31333c3f06e2f8fcc69b19da47140b4d0fbd4
yata74 <yata74@rambler.ru>
2020-04-16 12:36:49 +0500

    Coder

---
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -0,0 +1,52 @@
+package ru.progwards.java1.lessons.io1;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Coder {
+    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) throws Exception {
+
+        try {
+            FileReader file1 = new FileReader(inFileName);
+            Scanner scanner = new Scanner(file1);
+            FileWriter file2 = new FileWriter(outFileName);
+
+            try {
+                while (scanner.hasNextLine()) {
+                    String str = scanner.nextLine();
+                    char mass[] = str.toCharArray();
+                    for (int i = 0; i < mass.length; i++) {
+                        char a1 = code[mass[i]];
+                        file2.write(a1);
+                    }
+                }
+            } finally {
+                file1.close();
+                file2.close();
+            }
+        } catch (Exception e) {
+            LogFileWrite.log(e.getMessage());
+        }
+    }
+
+    public static class LogFileWrite {
+        private static String logName;
+
+        public static boolean log(String str) {
+            try {
+                FileWriter logFile = new FileWriter(logName, true);
+                try {
+                    logFile.write(str + "\n");
+                } finally {
+                    logFile.close();
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+                return false;
+            }
+            return true;
+        }
+    }
+}
