3fb1a679d50924dccbd8ceeea522522aec96e0fe
yata74 <yata74@rambler.ru>
2020-05-11 11:48:29 +0500

    Pyramid

--- src/ru/progwards/java1/lessons/abstractnum/Pyramid.java
+++ src/ru/progwards/java1/lessons/abstractnum/Pyramid.java
@@ -1,17 +1,17 @@
 package ru.progwards.java1.lessons.abstractnum;
-//
-//public class Pyramid extends Figure3D {
-//    public Pyramid (Number segment){
-//        super(segment);
-//
-//    }
-//    @Override
-//    public Number volume(){
-//  //TODO      return segment.mul(segment).mul(segment).div(3);
-//    }
-//
-//    @Override
-//    public String toString(){
-//        return String.valueOf(volume());
-//        }
-//}
+
+public class Pyramid extends Figure3D {
+    public Pyramid (Number segment){
+        super(segment);
+
+    }
+    @Override
+    public Number volume(){
+       return segment.mul(segment).mul(segment).div(segment.newNumber(String.valueOf(3)));
+    }
+
+    @Override
+    public String toString(){
+        return String.valueOf(volume());
+        }
+}
