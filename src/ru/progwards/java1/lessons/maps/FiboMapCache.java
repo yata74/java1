ffba52a44620ac15e17ce7408cc0ce77ca54ce1c
yata74 <yata74@rambler.ru>
2020-06-15 17:59:02 +0500

    FiboMapCache

---
+++ src/ru/progwards/java1/lessons/maps/FiboMapCache.java
@@ -0,0 +1,69 @@
+package ru.progwards.java1.lessons.maps;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FiboMapCache {
+
+    private Map<Integer, BigDecimal> fiboCache;
+    private boolean cacheOn;
+    public FiboMapCache(boolean cacheOn) {
+        this.cacheOn = cacheOn;
+        if (cacheOn == true) {
+            fiboCache = new HashMap<>();
+        }
+    }
+
+    public static BigDecimal fibonacci(int n){
+        BigDecimal sum = BigDecimal.ZERO;
+        BigDecimal b1;
+        BigDecimal b2 = BigDecimal.ONE;
+        int i = 1;
+        do {
+            b1 = b2;
+            b2 = sum;
+            sum = b1.add(b2);
+            i++;
+        }while (i<=n);
+
+        return sum;
+    }
+
+    public  BigDecimal fiboNumber(int n){
+        if (cacheOn ==true){
+            if (fiboCache.containsKey(n)){
+                return fiboCache.get(n);
+            }
+        }
+        BigDecimal a = fibonacci(n);
+        fiboCache.put(n,a);
+        return a;
+    }
+//    очищаем кэш
+    public void clearCahe(){
+        fiboCache.clear();
+//        fiboCache=null;
+        cacheOn=false;
+    }
+    public static void test(){
+        var startTime = new Date().getTime();
+        FiboMapCache obj = new FiboMapCache(true);
+        for (int i=1; i<=1000;i++){
+            obj.fiboNumber(i);
+        }
+
+        var startTime1 = new Date().getTime();
+        obj.clearCahe();
+        for (int i=1; i<=1000;i++){
+            obj.fiboNumber(i);
+        }
+        System.out.println("fiboNumber cacheOn=true время выполнения: "+(new Date().getTime() - startTime));
+        System.out.println("fiboNumber cacheOn=false время выполнения: "+(new Date().getTime() - startTime1));
+        }
+
+    public static void main(String[] args) {
+        test();
+    }
+    }
